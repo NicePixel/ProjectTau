32d5fcd4c7c3bc95f5054e3b61f338de90d17150
Aleksandar Urosevic <alek.sandar0@yandex.com>
2019-02-10 22:01:24 +0100

    Remove unnecessary variable in the font shader

--- bin/data/shaders/font_frg.glsl
+++ bin/data/shaders/font_frg.glsl
@@ -15,11 +15,12 @@ void main()
 	float movex = pos.x - totaltime * RAINBOW_SPEED;
 	vec2 posn = vec2(movex / 800.0, pos.y / 600.0);
 	
-	float t = abs(sin(totaltime));
 	float r = abs(sin(posn.x*RAINBOW_SCALE + 0));
 	float g = abs(sin(posn.x*RAINBOW_SCALE + PI/4));
 	float b = abs(sin(posn.x*RAINBOW_SCALE + 2*PI/4.0));
 	vec4 textColor = vec4(r, g, b, 1.0);
+	
     vec4 sampled = vec4(1.0, 1.0, 1.0, texture(texture0, TexCoords).r);
-    color = textColor * sampled;
+    
+	color = textColor * sampled;
 } 
