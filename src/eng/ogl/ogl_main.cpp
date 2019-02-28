65c62b189fe88ed64ac1eab61a6b14edcc0626ec
Aleksandar Urosevic <alek.sandar0@yandex.com>
2019-02-28 21:23:46 +0100

    use existing function for enabling depth testing

--- src/eng/ogl/ogl_main.cpp
+++ src/eng/ogl/ogl_main.cpp
@@ -50,13 +50,12 @@ void tau_gra_init(const char* windowTitle)
 
 	// Initial state
 	glClearColor(videoinfo.clear_r, videoinfo.clear_g, videoinfo.clear_b, 1.0f);
-	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 	glCullFace(GL_BACK);
-	glActiveTexture(GL_TEXTURE0);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // Disable byte-alignment restriction
+	tau_gra_enableDepthTest();
 	
 	// Call other initialization functions
 	tau_gra_font_init();
